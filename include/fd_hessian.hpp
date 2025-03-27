7c8db6062ba16b54e16da4cd43c2edc14645b36e
alep <alessandro.pedone02@gmail.com>
2025-03-27 11:09:16 +0100

    Parallel implementation of fd_hessian

--- include/fd_hessian.hpp
+++ include/fd_hessian.hpp
@@ -19,33 +19,38 @@
  * auto d  = d4(x0); // Computes the Hessian of f at x0 (x0 is a vector of size 2)
  */
 
-template <typename F, typename T, typename DT = DifferenceType::Centered>
-std::function<matrix_type(const vector_type &)> hessian(const F &f, const T &h)
-{
-    return [=](const vector_type &x) -> matrix_type
-    {
-        // Initialize the Hessian matrix
-        matrix_type hessian = matrix_type::Zero(x.size(), x.size());
-
-        // Compute the gradient with finite differences (DT type)
-        auto grad = gradient<decltype(f), double, DT>(f, h);
-
-        // Compute the Hessian
-        for (int i = 0; i < x.size(); ++i)
-        {
-            // The i-th component of the gradient
-            auto grad_i = [=](const vector_type &x)
-            { return grad(x)(i); };
-
-            // Gradient of the i-th component of the gradient of f (other type of finite differences)
-            auto grad_grad_i = gradient<decltype(f), double, typename DT::otherType>(f, h);
-
-            // Compute and insert the i-th row
-            hessian.row(i) = grad_grad_i(x);
-        }
-
-        return hessian;
-    };
-}
+ template <typename F, typename T, typename DT = DifferenceType::Centered>
+ std::function<matrix_type(const vector_type &)> hessian(const F &f, const T &h)
+ {
+     return [=](const vector_type &x) -> matrix_type
+     {
+         const int_type n = x.size();
+         matrix_type hessian = matrix_type::Zero(n, n);
+ 
+         // Create an index vector [0, 1, 2, ..., n-1]
+         std::vector<index_type> indices(n);
+         std::iota(indices.begin(), indices.end(), 0);
+ 
+         // Compute each Hessian row in parallel
+         std::for_each(std::execution::par, indices.begin(), indices.end(), [&](index_type i)
+         {
+             // Define grad_i: the i-th component of the gradient of f.
+             auto grad_i = [=](const vector_type &y) -> scalar_type {
+                 // Compute the full gradient of f at y, then select component i.
+                 auto grad = gradient<decltype(f), scalar_type, DT>(f, h);
+                 return grad(y)(i);
+             };
+ 
+             // Now compute the gradient of grad_i, using the alternate finite difference type.
+             auto grad_grad_i = gradient<decltype(grad_i), double, typename DT::otherType>(grad_i, h);
+             
+             // Compute the i-th row of the Hessian as the gradient of grad_i at x.
+             hessian.row(i) = grad_grad_i(x);
+         });
+ 
+         return hessian;
+     };
+ }
+ 
 
 #endif // FD_HESSIAN_HPP
